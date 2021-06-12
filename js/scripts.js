7dc9895163eb9214faf576796a32e86e9b506cd8
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2021-06-12 22:08:52 +0200

    create pokemonList variable and add objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-let favFood ="pizza";
-document.write(favFood);
-alert("Hello World.");
+let pokemonList = [
+  {name: "Spritzee", heightCentimeters: 20.3, types: "Fairy", category: "Perfume"},
+  {name: "Gloom", heightCentimeters: 78.7, types: ["Grass", "Poison"], category: "Weed"},
+  {name: "Haunter", heightCentimeters: 160,types: ["Ghost", "Poison"], category: "Gas"},
+  {name: "Charmander", heightCentimeters: 61, types: "Fire", category: "Lizard"}
+  ];

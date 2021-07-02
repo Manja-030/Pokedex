c5a46fb70cd7095e62be827f61d26399c2e47035
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2021-07-02 09:55:48 +0200

    created loop to iterate over items of pokemonList
    name an height of Pokémons are listed on website
    biggest Pokémon is highlighted with a comment

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,13 @@ let pokemonList = [
   {name: "Haunter", heightCentimeters: 160,types: ["Ghost", "Poison"], category: "Gas"},
   {name: "Charmander", heightCentimeters: 61, types: "Fire", category: "Lizard"}
   ];
+
+
+for (let i = 0; i < pokemonList.length; i++){
+  let big = " - Wow, that´s big!";
+  if (pokemonList[i].heightCentimeters > 100){
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].heightCentimeters + "cm)" + big + "<br>");
+  } else{
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].heightCentimeters + "cm)" + "<br>");
+  }
+}

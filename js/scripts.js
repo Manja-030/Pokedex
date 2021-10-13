1d9b8b76209a2673db55a89fcbdaacd231a7ada2
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2021-10-13 13:52:43 +0200

    added navbar

--- js/scripts.js
+++ js/scripts.js
@@ -144,15 +144,15 @@ pokemonRepository.loadList().then(function () {
 
 let mybutton = document.getElementById("btn-back-to-top");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 50px from the top of the document, show the button
 window.onscroll = function () {
   scrollFunction();
 };
 
 function scrollFunction() {
   if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
+    document.body.scrollTop > 50 ||
+    document.documentElement.scrollTop > 50
   ) {
     mybutton.style.display = "block";
   } else {

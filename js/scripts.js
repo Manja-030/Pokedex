efbd12c4fa148e6d27a11fd5f3cdffebc22563ee
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2021-10-17 23:38:33 +0200

    linted code

--- js/scripts.js
+++ js/scripts.js
@@ -162,7 +162,7 @@ pokemonRepository.loadList().then(function() {
   });
 });
 
-let mybutton = document.getElementById('btn-back-to-top');
+let myButton = document.getElementById('btn-back-to-top');
 
 // When the user scrolls down 50px from the top of the document, show the button
 window.onscroll = function() {
@@ -171,13 +171,13 @@ window.onscroll = function() {
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    mybutton.style.display = 'block';
+    myButton.style.display = 'block';
   } else {
-    mybutton.style.display = 'none';
+    myButton.style.display = 'none';
   }
 }
 // When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener('click', backToTop);
+myButton.addEventListener('click', backToTop);
 
 function backToTop() {
   document.body.scrollTop = 0;
